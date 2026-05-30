defc7c0174fef4ae20ffdfb2b3cea5a611599401
Tejaswa <tejaswa.8568@gmail.com>
2026-05-31 00:39:22 +0530

    Use Streamlit secrets

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -1,15 +1,28 @@
-import os
-
+'''import os
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 def get_llm():
 
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        google_api_key=os.getenv("GEMINI_API_KEY"),
+        temperature=0.3
+    )
+
+    return llm'''
+
+import streamlit as st
+from langchain_groq import ChatGroq
+
+
+def get_llm():
+
     llm = ChatGroq(
         model="llama-3.3-70b-versatile",
-        groq_api_key=os.getenv("GROQ_API_KEY"),
+        groq_api_key=st.secrets["GROQ_API_KEY"],
         temperature=0.1
     )
 
